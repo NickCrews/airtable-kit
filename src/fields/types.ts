7097c63028da5eb53bdbe3413aea4682b887e666
Nick Crews <nicholas.b.crews@gmail.com>
2025-11-02 22:14:26 -0700

    chore: lowercase FieldSchema names to match the FieldType

--- src/fields/types.ts
+++ src/fields/types.ts
@@ -1,39 +1,39 @@
 import * as fields from "./index.ts";
 
-const Field = {
-  AiText: null as unknown as fields.AiText,
-  AutoNumber: null as unknown as fields.AutoNumber,
-  Barcode: null as unknown as fields.Barcode,
-  Button: null as unknown as fields.Button,
-  Checkbox: null as unknown as fields.Checkbox,
-  Count: null as unknown as fields.Count,
-  CreatedBy: null as unknown as fields.CreatedBy,
-  CreatedTime: null as unknown as fields.CreatedTime,
-  Currency: null as unknown as fields.Currency,
-  Date: null as unknown as fields.Date,
-  DateTime: null as unknown as fields.DateTime,
-  Duration: null as unknown as fields.Duration,
-  Email: null as unknown as fields.Email,
-  ExternalSyncSource: null as unknown as fields.ExternalSyncSource,
-  Formula: null as unknown as fields.Formula,
-  LastModifiedBy: null as unknown as fields.LastModifiedBy,
-  LastModifiedTime: null as unknown as fields.LastModifiedTime,
-  MultilineText: null as unknown as fields.MultilineText,
-  MultipleAttachments: null as unknown as fields.MultipleAttachments,
-  MultipleCollaborators: null as unknown as fields.MultipleCollaborators,
-  MultipleLookupValues: null as unknown as fields.MultipleLookupValues,
-  MultipleRecordLinks: null as unknown as fields.MultipleRecordLinks,
-  MultipleSelects: null as unknown as fields.MultipleSelects,
-  Number: null as unknown as fields.Number,
-  Percent: null as unknown as fields.Percent,
-  PhoneNumber: null as unknown as fields.PhoneNumber,
-  Rating: null as unknown as fields.Rating,
-  RichText: null as unknown as fields.RichText,
-  Rollup: null as unknown as fields.Rollup,
-  SingleCollaborator: null as unknown as fields.SingleCollaborator,
-  SingleLineText: null as unknown as fields.SingleLineText,
-  SingleSelect: null as unknown as fields.SingleSelect,
-  Url: null as unknown as fields.Url,
+const FIELD_SCHEMAS = {
+  aiText: null as unknown as fields.AiText,
+  autoNumber: null as unknown as fields.AutoNumber,
+  barcode: null as unknown as fields.Barcode,
+  button: null as unknown as fields.Button,
+  checkbox: null as unknown as fields.Checkbox,
+  count: null as unknown as fields.Count,
+  createdBy: null as unknown as fields.CreatedBy,
+  createdTime: null as unknown as fields.CreatedTime,
+  currency: null as unknown as fields.Currency,
+  date: null as unknown as fields.Date,
+  dateTime: null as unknown as fields.DateTime,
+  duration: null as unknown as fields.Duration,
+  email: null as unknown as fields.Email,
+  externalSyncSource: null as unknown as fields.ExternalSyncSource,
+  formula: null as unknown as fields.Formula,
+  lastModifiedBy: null as unknown as fields.LastModifiedBy,
+  lastModifiedTime: null as unknown as fields.LastModifiedTime,
+  multilineText: null as unknown as fields.MultilineText,
+  multipleAttachments: null as unknown as fields.MultipleAttachments,
+  multipleCollaborators: null as unknown as fields.MultipleCollaborators,
+  multipleLookupValues: null as unknown as fields.MultipleLookupValues,
+  multipleRecordLinks: null as unknown as fields.MultipleRecordLinks,
+  multipleSelects: null as unknown as fields.MultipleSelects,
+  number: null as unknown as fields.Number,
+  percent: null as unknown as fields.Percent,
+  phoneNumber: null as unknown as fields.PhoneNumber,
+  rating: null as unknown as fields.Rating,
+  richText: null as unknown as fields.RichText,
+  rollup: null as unknown as fields.Rollup,
+  singleCollaborator: null as unknown as fields.SingleCollaborator,
+  singleLineText: null as unknown as fields.SingleLineText,
+  singleSelect: null as unknown as fields.SingleSelect,
+  url: null as unknown as fields.Url,
 };
 
 /**
@@ -53,7 +53,7 @@ const Field = {
  *    During Zod validator generation, this is added as a description to the Zod schema,
  *    so that it shows up in any generate JSON Schema or OpenAPI docs.
  */
-export type FieldSchema = typeof Field[keyof typeof Field];
+export type FieldSchema = typeof FIELD_SCHEMAS[keyof typeof FIELD_SCHEMAS];
 /**
  * One of the string type codes from the [Airtable API](https://airtable.com/developers/web/api/field-model), eg 'singleSelect'
  */
